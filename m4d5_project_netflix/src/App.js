ff4e7f883995987cfaa1ea2c334fcee2f20bf3a7
Helena Nel <mailhedri@gmail.com>
2021-06-04 17:15:22 +0200

    comment out back office

--- m4d5_project_netflix/src/App.js
+++ m4d5_project_netflix/src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
-import BackOffice from "./component/BackOffice";
+// import BackOffice from "./component/BackOffice";
 import Carousell from "./component/Carousell";
 import "./App.css";
 import Search from "./component/SearchComponent/Search";
+<<<<<<< Updated upstream
 import NavbarNetflix from "./component/Navbar/NavbarNetflix";
+=======
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <NavbarNetflix />
+=======
+>>>>>>> Stashed changes
       {/* add the book id to this */}
-      <BackOffice id="1621579832" />
+      {/* <BackOffice id="1621579832" /> */}
 
       <Search />
       <Carousell search={"Harry Potter"} />
